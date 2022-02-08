92939605ed9c5b282e2c09869599f65295decea3
Vikas Rathore <vikasrathore@cedcoss.com>
2022-02-08 15:18:51 +0530

    Adding Edit Feature using JavaScript

--- src/script.js
+++ src/script.js
@@ -1,24 +1,5 @@
 const Array=[];
-
-//adding new product in the products function
-function products(){
-    var product = fetch();
-    if(checkData(product.productId,product.productName,product.productPrice)==0){
-            Array.push(product);
-            var table = document.getElementById('mytable');
-            var row = table.insertRow(-1);
-            var cell1 = row.insertCell(0);
-            var cell2 = row.insertCell(1);
-            var cell3 = row.insertCell(2);
-            for(var i=0; i<Array.length ; i++){
-                cell1.innerHTML = Array[i].productId;
-                cell2.innerHTML = Array[i].productName;
-                cell3.innerHTML = "USD " + Array[i].productPrice;
-            
-            }
-        }
-}
-
+document.getElementById("updateProduct").style.display = "none";
 
 //fetching input field data
 function fetch(){
@@ -31,15 +12,22 @@ function fetch(){
         "productPrice" : product_price};
 }
 
-//checking Data types of input field
-
+//Validate data
 function checkData(productId,productName, productPrice){
     var flag=0;
+   /*  for(var i=0;i<Array.length;i++){
+        if(Array[i].productId==productId){
+            flag =1;
+            alert("Product Id exists , it should be unique");
+        }
+    } */
     if(isNaN(productId)){
         flag=1;
         alert("Product Id should be integer");
-    }
-    console.log(typeof(productName))
+        }
+    
+    
+  
     if(!isNaN(productName)){
         flag=1;
         alert("Product Name should be String")
@@ -48,9 +36,91 @@ function checkData(productId,productName, productPrice){
         flag=1;
         alert("Product Price should be Number")
     }
-    console.log(flag);
+   
     return flag ;
     
 
 }
+
+//displaying Html elements
+function display(){
+    var product = fetch();
+    if(checkData(product.productId,product.productName,product.productPrice)==0){
+            Array.push(product);
+            var table = document.getElementById('mytable');
+            var row = table.insertRow(-1);
+            var cell1 = row.insertCell(0);
+            var cell2 = row.insertCell(1);
+            var cell3 = row.insertCell(2);
+            var cell4 = row.insertCell(3);
+            for(var i=0; i<Array.length ; i++){
+                cell1.innerHTML = Array[i].productId;
+                cell2.innerHTML = Array[i].productName;
+                cell3.innerHTML = "USD " + Array[i].productPrice;
+                cell4.innerHTML = '<a href="#"onclick="editForm('+Array[i].productId+')" >Edit</a>';
+            
+            }
+        }
+}
+
+//adding new product in the products function
+function addProducts(){
+
+    display(); 
+}
+
+
+//run loop to get product
+function getProduct(productId){
+    console.log(productId);
+    console.log(productId);
+    console.log(productId);
+    for(var i=0 ; i<Array.length;i++){
+        if(productId==Array[i].productId){
+            return Array[i];
+        }
+        else{
+            console.log("Not found");
+        }
+    }
+}
+
+//update where id matches
+function updateProduct(){
+    var product =getProduct(productId);
+    for(var i=0;i<Array.length;i++){
+        if(productId==Array[i].productId){
+            Array[i].pop();
+            Array.unshift({
+                "productId" : product.productId,
+                "productName" : product.productName,
+                "productPrice" : product.productPrice
+            })
+            console.log(Array);
+        }
+    }
+    
+    display();
+    
+}
+
+function editForm(productId){
+    var product = getProduct(productId);
+    console.log(product);
+    document.getElementById("updateProduct").style.display="block";
+    document.getElementById("addProduct").style.display="none";
+    document.getElementById("productId").innerHTML=product.productId.value;
+    document.getElementById("productName").innerHTML=product.productName;
+    document.getElementById("productPrice").innerHTML=product.productPrice;
+    return product;
+}
+
+
+
+
+
+
+
+
+
 
